15d173fb69571ec51d9999eee98b01d7f4ef7698
katringht <katrin_tko@mail.ru>
2021-04-09 20:52:10 +0300

    little start

---
+++ dict/dict/view/Slide.swift
@@ -0,0 +1,20 @@
+//
+//  Slide.swift
+//  dict
+//
+//  Created by Ekaterina Tarasova on 09.04.2021.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
